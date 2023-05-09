07fbd6f8ecd1bf499b4c3f825962390d4d5084f4
Aid Puska <aidpuska29@gmail.com>
2023-05-09 19:27:52 +0200

    Changed fetching site

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -29,7 +29,7 @@ const Main = () => {
       setLoading(true)
 
       try {
-        const res = await fetch("http://localhost:8080/api/v1/post", {
+        const res = await fetch("https://dall-e-aid.onrender.com//api/v1/post", {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json'
